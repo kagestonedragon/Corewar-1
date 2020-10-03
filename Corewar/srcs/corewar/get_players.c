c32bf8c294b48261ad619bdb0986953cfffcebdf
Владимир Ляшенко <vladimirlyashenko@192.168.1.7>
2020-10-03 19:46:14 +0300

    Added comments

--- Corewar/srcs/corewar/get_players.c
+++ Corewar/srcs/corewar/get_players.c
@@ -36,76 +36,76 @@ void	is_player(t_vm *v, int live)
 		}
 }
 
-void	save_player(char *file, t_vm *v, int i, int num)
+void	save_player(char *file, t_vm *v, int i, int num) // сохраняем игрока в структуры
 {
 	int	fd;
 	int len;
 
-	if ((fd = open(file, O_RDONLY)) != -1)
+	if ((fd = open(file, O_RDONLY)) != -1) // открываем его файл
 	{
-		read(fd, &(v->p[i].exec_magic), sizeof(int));
-		read(fd, &(v->p[i].name), PROG_NAME_LENGTH);
-		lseek(fd, 8, SEEK_CUR);
-		read(fd, &(v->p[i].comment), COMMENT_LENGTH);
-		lseek(fd, 4, SEEK_CUR);
-		len = read(fd, &(v->p[i].code), CHAMP_MAX_SIZE);
-		v->p[i].nplayer = num;
-		v->p[i].prog_len = len;
+		read(fd, &(v->p[i].exec_magic), sizeof(int)); // читаем magic header
+		read(fd, &(v->p[i].name), PROG_NAME_LENGTH); // читаем имя чемпиона
+		lseek(fd, 8, SEEK_CUR); // пропускаем пропуск
+		read(fd, &(v->p[i].comment), COMMENT_LENGTH); // читаем комментарий чемпиона
+		lseek(fd, 4, SEEK_CUR); // пропускаем пропуск
+		len = read(fd, &(v->p[i].code), CHAMP_MAX_SIZE); // читаем код чемпиона
+		v->p[i].nplayer = num; // пишем номер чемпиона
+		v->p[i].prog_len = len; // пишем длину кода чемпиона
 		v->p[i].nblive = 0;
 		v->p[i].last_live_cycle = 0;
 	}
 	else
-		xerror("Error: cannot open file", -1);
-	close(fd);
+		xerror("Error: cannot open file", -1); // иначе ошибка не смогли открыть файл
+	close(fd); // закрыли файл
 }
 
-void	get_player_custom(char **argv, int i, t_vm *v)
+void	get_player_custom(char **argv, int i, t_vm *v) // функция кастомного считывания чемпиона (после флага -n)
 {
 	int num;
 	int j;
 
-	num = ft_atoi(argv[i + 1]);
+	num = ft_atoi(argv[i + 1]); // считываем номер с помощью фещш
 	j = 0;
-	while (j < v->nplayer)
+	while (j < v->nplayer) // проходим по номерам игроков
 		if (v->p[j++].nplayer == num)
-			xerror("Error: player number already assigned", -1);
-	if (is_valid_player(argv[i + 2]))
+			xerror("Error: player number already assigned", -1); // ошибка если такой номер уже есть
+	if (is_valid_player(argv[i + 2])) // проверяем что игрок валиден
 	{
-		if (v->nplayer < MAX_PLAYERS)
-			save_player(argv[i + 2], v, v->nplayer, num);
+		if (v->nplayer < MAX_PLAYERS) // если кол-во игроков не превышено
+			save_player(argv[i + 2], v, v->nplayer, num); // сохраняем
 		else
-			xerror("Error: too many players", -1);
+			xerror("Error: too many players", -1); // иначе ошибка - слишком много игроков
 		v->nplayer++;
 		v->nplayer_cust++;
 	}
 	else
-		xerror("Error: invalid champion", -1);
+		xerror("Error: invalid champion", -1); // иначе ошибка - не валидный чемпион
 }
 
-void	get_player(char **argv, int i, t_vm *v)
+void	get_player(char **argv, int i, t_vm *v) // функция считывания чемпиона
 {
 	int j;
 	int num;
 	int doublon;
 
 	doublon = 0;
-	num = -v->nplayer - 1;
+	num = -v->nplayer - 1; // присваиваем первичный номер
 	j = 0;
-	while (j < v->nplayer)
+	while (j < v->nplayer) // проходим по номерам игроков
 	{
-		if (v->p[j++].nplayer == num)
-			doublon = 1;
-		num -= doublon ? 1 : 0;
+		if (v->p[j++].nplayer == num) // проверяем есть ли чеспион с таким же номером
+			doublon = 1; // есть двойник
+		num -= doublon ? 1 : 0; // получаем номер игрока (в случае если у нас будет -n флаг нам нужно это сделать, чтобы не было двух игроков с одинаковыми номерами)
 		doublon = 0;
 	}
-	if (is_valid_player(argv[i]))
+	if (is_valid_player(argv[i])) // проверяем валидный ли чемпион
 	{
-		if (v->nplayer < MAX_PLAYERS)
-			save_player(argv[i], v, v->nplayer, num);
+		if (v->nplayer < MAX_PLAYERS) // если номер игрока меньше чем максимальное кол-во игрокоа
+			save_player(argv[i], v, v->nplayer, num); // сохраняем
 		else
-			xerror("Error: too many players", -1);
+			xerror("Error: too many players", -1); // иначе ошибка (слишком много игроков)
 		v->nplayer++;
 	}
 	else
-		xerror("Error: invalid champion", -1);
+		xerror("Error: invalid champion", -1); //  иначе ошибка (не валидный чемпион)
 }
